bc59c2be52d403e05e1c25921f938f227aca4801
Raymond Williams <msuraymond@gmail.com>
2026-04-13 15:30:29 +0100

    Be Techified Assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+let name = "Raymond Iorliam"
+let age = 17
+let favoriteLanguage = "JavaScript";
+
+console.log('My name is ' + name);
+console.log('I am ' + age + ' old');
+console.log('My favourite programming Language is ' + favoriteLanguage);
